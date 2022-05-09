17a76ccf89482de6e1e46898d2c6cf2aeefa0243
verysoyoung <50602551+verysoyoung@users.noreply.github.com>
2022-05-09 22:32:47 +0900

    ADD: loginsaga yield 추가

--- store/login/saga.js
+++ store/login/saga.js
@@ -3,3 +3,7 @@ import { asyncLoginActions } from './reducer'
 
 const baseUrl = '/login'
 const loginRequestSaga = ''
+
+export default function* loginSaga() {
+  yield takeLeading(asyncLoginActions.request, loginRequestSaga)
+}
