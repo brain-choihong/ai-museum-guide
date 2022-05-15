72c297f5ba894d36d5a8571bbe829bda12dd5492
verysoyoung <50602551+verysoyoung@users.noreply.github.com>
2022-05-15 18:18:36 +0900

    ADD: Users model 추가

--- models/User.js
+++ models/User.js
@@ -1,9 +1,19 @@
 import mongoose from 'mongoose'
 
 const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true, lowercase: true },
-  password: { type: String, required: true, trim: true },
-  nickname: { type: String, required: true },
+  email: {
+    type: String,
+    required: [true, 'Email is required!'],
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'password is required!'],
+    trim: true,
+  },
+  nickname: { type: String, required: [true, 'nickname is required!'] },
+  createdAt: { type: Date, default: Date.now },
 })
 
 export default mongoose.models.Register || mongoose.model('User', UserSchema)
--- pages/api/register/index.js
+++ pages/api/register/index.js
@@ -1,6 +1,27 @@
 import UserSchema from 'models/User'
+import 'models/dbConnect'
 
-export default async (req,res) => {
-  const {
+export default async (req, res) => {
+  const { method } = req
+
+  switch (method) {
+    case 'POST':
+      try {
+        const users = await UserSchema.create(req.body)
+        return res.status(200).json({
+          success: true,
+          data: users,
+        })
+      } catch (error) {
+        return res.status(400).json({
+          success: false,
+        })
+      }
+    default:
+      res.setHeaders('Allow', ['POST'])
+      return res
+        .status(405)
+        .json({ success: false })
+        .end(`Method ${method} Not Allowed`)
   }
 }
